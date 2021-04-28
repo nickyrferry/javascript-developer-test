0ec292008da02ae1e4dbf7fef6d3fa330271d120
Nicola Ferendinos <nicolaferendinos@gmail.com>
2021-04-28 10:15:10 +1000

    implement the getArnieQuotes function

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,21 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  let results = [];
+  const requests = urls.map(async (url) => await httpGet(url));
+
+  return Promise.all(requests)
+    .then((responses) => {
+      responses.forEach((response) => {
+        const element = JSON.parse(response.body);
+        if (response.status === 200)
+          results.push({ 'Arnie Quote': element.message })
+        else 
+          results.push({ 'FAILURE': 'Your request has been terminated' });
+        })
+        return results;
+    })
+    .catch((error) => console.log(`ERROR getting Arnie Quotes: ${error}`))
 };
 
 module.exports = {
